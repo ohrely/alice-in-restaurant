501c0c9b2799b11ac96444134d51dc6bd841984e
ohrely <rebaely@gmail.com>
2016-01-24 10:37:38 -0800

    Building out markov file.  Read text function done.

---
+++ markov.py
@@ -0,0 +1,15 @@
+def pull_text(file_path):
+    """Open and read file with source text."""
+    with open(file_path) as source_text:
+        read_text = source_text.read()
+
+    return read_text
+
+
+# strip text
+
+
+# break down into chains, store in dictionary
+
+
+# construct tweets - use while loop for now
