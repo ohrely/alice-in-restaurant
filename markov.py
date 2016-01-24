fe985f382d5ea970232903340d8e6314276c4773
ohrely <rebaely@gmail.com>
2016-01-24 10:59:23 -0800

    Strip text function built.  Added tests.

--- markov.py
+++ markov.py
@@ -1,15 +1,41 @@
+import doctest
+
+
 def pull_text(file_path):
-    """Open and read file with source text."""
+    """Open and read file with source text.
+
+    >>> pull_text("test.txt")
+    'All mimsy were the borogroves\\nAnd the mome raths outgrabe.'
+    """
+
     with open(file_path) as source_text:
         read_text = source_text.read()
 
     return read_text
 
 
-# strip text
+def strip_text(text):
+    """Strip odd characters from source text.
+
+    TODO: May choose to reintroduce some of these characters later.
+    TODO: Modularity!
+
+    >>> strip_text(pull_text("test.txt"))
+    'All mimsy were the borogroves And the mome raths outgrabe.'
+    """
+
+    text = " ".join(text.split("\n"))
+    text = " ".join(text.split("-"))
+
+    return text
 
 
 # break down into chains, store in dictionary
 
 
 # construct tweets - use while loop for now
+
+
+if __name__ == "__main__":
+
+    doctest.testmod(verbose=True)
